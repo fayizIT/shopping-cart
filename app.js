acab281b2538a1a985930b2a3ac82a53509d63f1
FIRST_NAME LAST_NAME <fayizcj@gmail.com>
2023-07-14 18:06:24 +0530

    new updates

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload());
-app.use(session({secret:"Key",cookie:{maxAge:6000}}))
+app.use(session({secret:"Key",cookie:{maxAge:60000}}))
 db.connect((err)=>{
   if(err){
     console.log("database is error"+err)
